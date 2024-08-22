9f34fdca608f9a642dd6aef435d37a8fc4893ca4
Divyanshu Yadav <143935228+coldzera630@users.noreply.github.com>
2024-08-22 23:42:57 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./styles.css";
 import "./Randompokermon.css";
 import Randompokemon from "./Randompokemon";
-// function Greed() {
-//   return <h1>HELLO</h1>;
-// }
 export default function App() {
   return (
     <div className="App">
